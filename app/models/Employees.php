52191d263779e2576728f6b95d5979853f4fa57e
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2019-06-07 23:32:50 -0400

    Changed models

--- app/models/Employees.php
+++ app/models/Employees.php
@@ -6,7 +6,7 @@ class Employees
 	[
 		["id" => 1, "name" => "Jonathan Sequeira", "age" => 27, "salary" => "$80,000"],
 		["id" => 2, "name" => "Orion Sequeira", "age" => 1, "salary" => "$800.00"],
-		["id" => 3, "name" => "Dungjai Sequeira", "age" => 29, "salary" => "$25,000"]
+		["id" => 3, "name" => "Dungjai Rodtim Sequeira", "age" => 29, "salary" => "$35,000"]
 	];
 
 	public function getEmployees () {
