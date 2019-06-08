23f85ab217c5a248ce0c7fbcb1915329ea94fec8
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2019-06-07 23:27:29 -0400

    Activity 2 Complete

--- app/controllers/PersonController.php
+++ app/controllers/PersonController.php
@@ -6,8 +6,18 @@ class PersonController extends Controller
 {
 	public function indexAction()
 	{
+		$people = new Employees();
+		$id = $this->request->get('id');
+		$peopleArray = $people->getEmployees();
+		$selected;
 
-		
+		foreach($peopleArray as $person){
+			if($person['id'] == $id) {
+				$selected = ["name" => $person['name'], "age" => $person['age'], "salary" => $person['salary']];
+			}
+		}
+
+		$this->view->person = $selected;
 	}
 
 }
